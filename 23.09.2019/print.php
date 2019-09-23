84aa397ba04a075c2604d439adcafc2d22400257
martin-ritsberg-khk <martin.ritsberg@khk.ee>
2019-09-23 11:59:27 +0300

    Teksti väljestamine print ja echo PHP-s, koos HTML-iga

--- 23.09.2019/print.php
+++ 23.09.2019/print.php
@@ -1,9 +1,22 @@
+<!doctype html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Teksti väljandamine PHP-s</title>
+</head>
+<body>
 <?php
 // printimine Print funktsiooni abil
 print "<h1>See on esimene php rida</h1>";
 print("See on teine php rida<br>");
-print "See on komas php rida<br>";
+print 'See on komas php rida<br>';
 /* see on mitme realine
 kommentaar */
 
-echo 'See on tekst, mis kirjutatud echo abil';
+echo "See on tekst, mis kirjutatud echo abil";
+?>
+</body>
+</html>
