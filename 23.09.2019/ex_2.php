32e98f92b7931287f62b14e5a948071af4009833
martin-ritsberg-khk <martin.ritsberg@khk.ee>
2019-09-23 12:37:26 +0300

    Ülesanne 2

---
+++ 23.09.2019/ex_2.php
@@ -0,0 +1,14 @@
+<?php
+$eesnimi = "Juku";
+$perenimi = "Tamm";
+$vanus = 23;
+$pikkus = 1.94;
+$kaal = 62;
+
+echo "Nimi on: ".$eesnimi." ".$perenimi;
+echo "<br>";
+echo "Vanus on: ".$vanus;
+echo "<br>";
+echo "Pikkus on: ".$pikkus." meetrit";
+echo "<br>";
+echo "Kaal on: ".$kaal." kilogrammi";
