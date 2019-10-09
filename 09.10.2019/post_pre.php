acef1884811593cd04b481577143f88eb4f7fd34
martin-ritsberg-khk <martin.ritsberg@khk.ee>
2019-10-09 15:26:12 +0300

    pärast ja eel

--- 09.10.2019/post_pre.php
+++ 09.10.2019/post_pre.php
@@ -1,7 +1,9 @@
 <?php
 //suurendamine ühe võrra
 $x = 10;
+//seda tehakse enne omistamist
 $y=++$x;
+//seda tehakse pärast omistamist(ei kehti x muutujale)
 //$y=$x++;
 echo "X = ".$x.'<br>';
 echo "Y = ".$y;
